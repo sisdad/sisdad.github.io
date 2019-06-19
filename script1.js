b98eb6aedbd3806323b181b3433533775e9220d9
sisdad <52007475+sisdad@users.noreply.github.com>
2019-06-19 16:03:42 +0200

    First website upoad

---
+++ script1.js
@@ -0,0 +1,55 @@
+ 
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+ // Images slide show of homepage
+
+  function NextImage() {
+    myImage.setAttribute("src",imgArray[index]);
+    if (index<6) {
+         index++;}
+    else index=0;
+    // body...
+  }
+  function PrevImage() {
+    myImage.setAttribute("src",imgArray[index]);
+    if (index>0) {
+         index--;}
+    else index=6;
+    // body...
+  }
+
+
+    function slideit(){
+        myImage.setAttribute("src",imgArray[index]);
+        if(index<6)
+            index++;
+        else
+            index=0;
+        setTimeout("slideit()",2000);
+        }
